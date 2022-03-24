f56c06f6ff84a722798853d76835b694218c030b
akash-pyramid <101701675+akash-pyramid@users.noreply.github.com>
2022-03-24 17:50:15 +0530

    Update conversion_controller.rb
    fix indentation

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -5,7 +5,6 @@ class ConversionController < ApplicationController
 
 	def convert_to_words
 		input_value = params[:input_value].to_i
-		number_to_word_obj = NumberToWordConverter.new
-		@result = number_to_word_obj.in_words(input_value)
+		@result = NumberToWordConverter.new.in_words(input_value)
 	end
 end
