609db56e6573fb027d8e950198256685aa613dcb
akash-pyramid <Akash.vishwakarma@pyramidconsultinginc.com>
2022-03-25 10:21:25 +0530

    updated test cases for incorrect o/p

--- spec/controllers/calculator_controller_spec.rb
+++ spec/controllers/calculator_controller_spec.rb
@@ -12,9 +12,16 @@ describe CalculatorController do
   end
 
   describe "POST #evaluate_expression" do
-    it "should route to evaluate_expression action" do
+    it "should evaluate #43 as o/p" do
       post :evaluate_expression, params: {input_value: "5*7+8"}
       expect(assigns(:result)).to eq(43)
+    end
+  end
+
+  describe "POST #evaluate_expression" do
+    it "should evaluate #43 as o/p" do
+      post :evaluate_expression, params: {input_value: "12+2*7+8"}
+      expect(assigns(:result)).to_not eq(43)
     end
   end
 end
--- spec/controllers/conversion_controller_spec.rb
+++ spec/controllers/conversion_controller_spec.rb
@@ -11,9 +11,16 @@ describe ConversionController do
   end
 
   describe "POST #convert_to_words" do
-    it "should route to convert_to_words action" do
+    it "should have correct o/p" do
       post :convert_to_words, params: {input_value: "67189"}
       expect(assigns(:result)).to eq("sixty seven thousand one hundred eighty nine")
+    end
+  end
+
+  describe "POST #convert_to_words" do
+    it "should have incorrect o/p" do
+      post :convert_to_words, params: {input_value: "67189"}
+      expect(assigns(:result)).to_not eq("sixty two seven thousand one hundred eighty nine")
     end
   end
 end
